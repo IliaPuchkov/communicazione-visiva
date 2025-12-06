c3bfd24734a89be7c6f2111611dc3f8bee8a6fec
Julia Gabryszewski <gabryszewski003@MacBookPro.station>
2025-12-06 05:21:36 +0100

    changes

--- js/carousel.js
+++ js/carousel.js
@@ -1,20 +1,39 @@
 const track = document.querySelector(".carousel-track");
-const slides = Array.from(track.children);
+const slides = document.querySelectorAll(".slide");
 const next = document.querySelector(".next");
 const prev = document.querySelector(".prev");
 
-let index = 0;
+let index = 1; // начинаем не с 0, а с первого РЕАЛЬНОГО слайда
 
-function update() {
-  track.style.transform = `translateX(-${index * 100}%)`;
+track.style.transform = `translateX(-${100 * index}%)`;
+
+function move() {
+  track.style.transition = "0.9s cubic-bezier(.25, .8, .25, 1)";
+  track.style.transform = `translateX(-${100 * index}%)`;
 }
 
 next.addEventListener("click", () => {
-  index = (index + 1) % slides.length;
-  update();
+  index++;
+  move();
+
+  if (index === slides.length - 1) {
+    setTimeout(() => {
+      track.style.transition = "none";
+      index = 1;
+      track.style.transform = `translateX(-${100 * index}%)`;
+    }, 900);
+  }
 });
 
 prev.addEventListener("click", () => {
-  index = (index - 1 + slides.length) % slides.length;
-  update();
+  index--;
+  move();
+
+  if (index === 0) {
+    setTimeout(() => {
+      track.style.transition = "none";
+      index = slides.length - 2;
+      track.style.transform = `translateX(-${100 * index}%)`;
+    }, 900);
+  }
 });
