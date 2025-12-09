5a4f947e6b9d0d4a26cb2852f2535d2a957a91e0
Julia Gabryszewski <gabryszewski003@MacBookPro.station>
2025-12-09 19:27:48 +0100

    changes

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,6 @@
 const navToggleBtn = document.querySelector(".cover__hb");
 const navCloseBtn = document.querySelector(".cover__hb-close");
-const navMenu = document.querySelector(".cover__hb-menu");
+const navMenu = document.querySelector(".cover-bottom");
 
 navToggleBtn.addEventListener("click", () => {
   navMenu.classList.add("hb-active");
